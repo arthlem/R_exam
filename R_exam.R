2b46b86380e0c131f4ab406e5533f8b13f40c534
Francois  Somville <francoissomville@mbpdefrancois3.cti.ichec.be>
2018-12-17 15:50:38 +0100

    Structure du fichier

--- R_exam.R
+++ R_exam.R
@@ -1,9 +1,19 @@
+# GIT
 #Lors de changements
-# git add . && git commit -m "Le message a envoyer" && git push
+#1. git add .
+#2. git commit -m "Le message a envoyer"
+#3. git push
 
 #Pour récupérer les changements
-# git pull
+#git pull
+#
+
+##CHARGEMENT DES DONNEES
 
 supermarkets <- read.csv2(file.choose(), header=TRUE, sep=",", dec=".") #charger le fichier CSV
 #Commande pour afficher le CSV des supermarchés
 #View(supermarkets)
+names(supermarkets)
+
+##PARTIE 1: STATISTIQUE DESCRIPTIVE
+
