9f4a5e153dbf865c8792c8ea3ff41bd5c6e69cc2
Arthur Lemoine <arthlem1@gmail.com>
2018-12-17 15:09:18 +0100

    France caff

--- R_exam.R
+++ R_exam.R
@@ -1,3 +1,4 @@
 supermarkets <- read.csv2(file.choose(), header=TRUE, sep=",", dec=".") #charger le fichier CSV
 #Commande pour afficher le CSV des supermarchés
 #View(supermarkets)
+#France fout le caff
