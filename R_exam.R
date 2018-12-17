15817f801a2ebdb90a748de5d9c5b84d3cf7bc36
Arthur Lemoine <arthlem1@gmail.com>
2018-12-17 15:22:10 +0100

    Commande en une ligne

--- R_exam.R
+++ R_exam.R
@@ -1,10 +1,8 @@
 #Lors de changements
-#1. git add .
-#2. git commit -m "Le message a envoyer"
-#3. git push
+# git add . && git commit -m "Le message a envoyer" && git push
 
 #Pour récupérer les changements
-#git pull
+# git pull
 
 supermarkets <- read.csv2(file.choose(), header=TRUE, sep=",", dec=".") #charger le fichier CSV
 #Commande pour afficher le CSV des supermarchés
