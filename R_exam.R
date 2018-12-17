5e82e6aaa8973571bcd09a4b9eec6886f26a76b1
Arthur Lemoine <arthlem1@gmail.com>
2018-12-17 15:12:49 +0100

    Ajout des commandes de récupération

--- R_exam.R
+++ R_exam.R
@@ -1,3 +1,11 @@
+#Lors de changements
+#1. git add .
+#2. git commit -m "Le message a envoyer"
+#3. git push
+
+#Pour récupérer les changements
+#git pull
+
 supermarkets <- read.csv2(file.choose(), header=TRUE, sep=",", dec=".") #charger le fichier CSV
 #Commande pour afficher le CSV des supermarchés
 #View(supermarkets)
