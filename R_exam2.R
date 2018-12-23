8d73b2dfa3c76a77af89eea0bcfb77f0336a8436
Arthur Lemoine <arthlem1@gmail.com>
2018-12-23 15:09:56 +0100

    add some functions

---
+++ R_exam2.R
@@ -0,0 +1,9 @@
+#Lors de changements
+# git add . && git commit -m "Le message a envoyer" && git push
+
+#Pour récupérer les changements
+# git pull
+
+supermarkets <- read.csv2(file.choose(), head=TRUE, sep=",") #charger le fichier CSV
+#Commande pour afficher le CSV des supermarchés
+View(supermarkets)
