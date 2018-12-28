1a5fab16d7e30e234ceabd27b0afc18fe313c3ef
Arthur Lemoine <arthlem1@gmail.com>
2018-12-28 12:14:26 +0100

    Putting all in the ass of Alexane

--- R_Online_Retail.R
+++ R_Online_Retail.R
@@ -8,11 +8,18 @@
 #git pull
 
 #-------------------------------------------ONLINE RETAIL CSV-------------------------------------------------
-
+#Libraries to install
+install.packages("ggplot2")
 install.packages("dplyr")
-install.packages("tidyverse")
+install.packages("lubridate")
+
+#Libraries to load
 library(dplyr)
-library(tidyverse)
+library(ggplot2)
+library(data.table)
+library(lubridate)
+#General constants
+turnoverByMonthScale <- 1/1000000
 
 #----A. IMPORT THE DATA----
 Onlineretail <- read.csv2(file.choose(), header=TRUE, sep=";", dec=".", row.names = NULL) #Load CSV File
@@ -68,12 +75,38 @@ round((135080/541909)*100,digit=2)
 #Remove Invoices beggining with C
 OnlineretailClean <- subset(OnlineretailClean, grepl("^(?!C).*$", OnlineretailClean$InvoiceNo, perl = TRUE))
 
-#Remove POST (postage)
+#Finish cleaning dataset in one line 
+# DataToRemove <- c('POST', 'D', 'C2', 'M', 'BANK CHARGES', 'PADS', 'DOT')
+# 
+# OnlineretailClean <- subset(OnlineretailClean, !(StockCode %in% DataToRemove))
+
+#Remove POSTAGE
 OnlineretailClean <- subset(OnlineretailClean, StockCode != "POST")
+
+#Remove Discount
+OnlineretailClean <- subset(OnlineretailClean, StockCode != "D")
+
+#Remove CARRIAGE
+OnlineretailClean <- subset(OnlineretailClean, StockCode != "C2")
+
+#Remove Manual
+OnlineretailClean <- subset(OnlineretailClean, StockCode != "M")
+
+#Remove Bank Charges
+OnlineretailClean <- subset(OnlineretailClean, StockCode != "BANK CHARGES")
+
+#Remove PADS TO MATCH ALL CUSHIONS 
+OnlineretailClean <- subset(OnlineretailClean, StockCode != "PADS")
+
+#Remove DOTCOM POSTAGE
+OnlineretailClean <- subset(OnlineretailClean, StockCode != "DOT")
 
 #Remove Duplicates
 OnlineretailUnique <- unique(OnlineretailClean)
 dim(OnlineretailClean)-dim(OnlineretailUnique)
+
+#Compute total revenue per row
+setDT(OnlineretailClean)[, TotalPrice := as.numeric(OnlineretailClean$UnitPrice)*OnlineretailClean$Quantity]
 
 #----ANALYSIS OF THE DATA - DESCRIPTIVE STATISTICS----
 
@@ -92,7 +125,7 @@ length(ListOfCustomers[!duplicated(ListOfCustomers), ])
 
 #5. Amount of purchases for each country
 PurchasesPerCountry <- aggregate(Onlineretail$Quantity, by=list(Category=Onlineretail$Country), FUN=sum)
-View(PurchasesPerCountry)
+#View(PurchasesPerCountry)
 PurchasesNotUk <- PurchasesPerCountry[-36,]
 
 #PieChart with all countries: TO DO -> Calc the % of sales from UK (!!)
@@ -104,8 +137,6 @@ pie(slices, labels = lbls, main="Pie Chart of Countries without UK")
 slicesNotUK <- PurchasesNotUk[[2]]
 lblsNotUK <- PurchasesNotUk[[1]]
 pie(slicesNotUK, labels = lblsNotUK, main="Pie Chart of Countries without UK")
-
-
 
 
 #Countries with the most returns
@@ -124,4 +155,60 @@ boxplot(SalesPerProduct[2], main= "Sales per product", horizontal = TRUE, outlin
 
 boxplot(Onlineretail, main= "Purchases", horizontal = TRUE, outline = FALSE,las=2)
 
+#Invoices per month in 2011
 
+#Check format of dates
+OnlineretailClean$InvoiceDate <- mdy_hm(OnlineretailClean$InvoiceDate)
+
+#Creating object for date's year
+OnlineretailClean$InvoiceYear <- year(OnlineretailClean$InvoiceDate)
+#Creating object for date's month
+OnlineretailClean$InvoiceMonth <- month(OnlineretailClean$InvoiceDate,label=T)
+#Creating object for date's day
+OnlineretailClean$InvoiceWeekday <- wday(OnlineretailClean$InvoiceDate, label=T)
+#Creating object for date's hour
+OnlineretailClean$InvoiceHour <- hour(OnlineretailClean$InvoiceDate)
+View(OnlineretailClean)
+#Number of invoices per month in 2011
+#Filter to select year 2011 and count invoices for each month
+monthData <- OnlineretailClean %>% 
+  dplyr::filter(InvoiceYear==2011) %>% 
+  count(InvoiceMonth)
+
+ggplot(monthData, aes(InvoiceMonth, n)) +  #plot the number of invoices per day               
+  geom_bar(stat="identity", fill="steelblue")+
+  geom_text(aes(label=n), vjust=1.6, color="white", size=3.5)+
+  labs(x="Month", y="Number of invoices")
+
+#Number of invoices per day in 2011
+#Filter to select year 2011 and count invoices for each day of the week
+dayData <- OnlineretailClean %>% 
+  dplyr::filter(InvoiceYear==2011) %>% 
+  count(InvoiceWeekday)
+
+ggplot(dayData, aes(InvoiceWeekday, n)) +  #plot the number of invoices per day               
+  geom_bar(stat="identity", fill="steelblue")+
+  geom_text(aes(label=n), vjust=1.6, color="white", size=3.5)+
+  labs(x="Week", y="Number of invoices")
+
+#Number of invoices per hour in 2011
+#Filter to select year 2011 and count invoices for each hour of the day
+hourData <- OnlineretailClean %>% 
+  dplyr::filter(InvoiceYear==2011) %>% 
+  count(InvoiceHour)
+
+ggplot(hourData, aes(InvoiceHour, n)) +  #plot the number of invoices per day               
+  geom_bar(stat="identity", fill="steelblue")+
+  geom_text(aes(label=n), vjust=1.6, color="white", size=3.5)+
+  labs(x="hour", y="Number of invoices")
+
+#Turnover per month in 2011
+SalesData <- OnlineretailClean %>%
+  dplyr::filter(InvoiceYear == 2011) %>%
+  group_by(InvoiceMonth) %>%
+  summarise(CA = sum(TotalPrice))
+
+ggplot(SalesData, aes(InvoiceMonth, CA*turnoverByMonthScale)) +              
+  geom_bar(stat="identity", fill="steelblue")+
+  geom_text(aes(label=format(round(CA*turnoverByMonthScale, 2), nsmall = 2)), vjust=1.6, color="white", size=3.5)+
+  labs(x="Month", y="Turnover in million")
