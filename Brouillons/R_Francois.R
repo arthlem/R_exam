7c7b0b763ed79c17049c05c4dc7a8c883169d735
Francois  Somville <francoissomville@MacBook-Pro-de-Francois-3.local>
2018-12-29 13:43:56 +0100

    Arthur pue du cul

---
+++ Brouillons/R_Francois.R
@@ -0,0 +1,19 @@
+# GIT
+#Lors de changements
+#1. git add .
+#2. git commit -m "Le message a envoyer"
+#3. git push
+
+#Pour récupérer les changements
+#git pull
+#
+
+#-----Feuille de brouillon François-----
+
+
+
+
+
+
+
+
